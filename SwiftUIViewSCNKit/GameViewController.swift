455a697607986adfbb46f2869ea7a042c11c3494
Sarang Borude <sarang.borude@gmail.com>
2020-12-29 11:53:34 -0800

    Fixed Camera angle

--- SwiftUIViewSCNKit/GameViewController.swift
+++ SwiftUIViewSCNKit/GameViewController.swift
@@ -58,18 +58,13 @@ class GameViewController: UIViewController {
         
         // configure the view
         scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+
         
         createHostingController(planeNode: planeNode)
     }
     
     func createHostingController(planeNode: SCNNode) {
-        
 
-        
         // create a hosting controller with SwiftUI view
         let arVC = UIHostingController(rootView: SwiftUIARCardView())
         
@@ -106,41 +101,6 @@ class GameViewController: UIViewController {
         hostingVC.view.backgroundColor = UIColor.clear
     }
     
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
     
     override var shouldAutorotate: Bool {
         return true
